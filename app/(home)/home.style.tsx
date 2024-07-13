9051701ee500844521c085fa7cb7fc54bde4c649
Isaque Chaves <isaque.ramosch@gmail.com>
2024-07-13 15:10:22 -0300

    feat: add Home page

--- app/(home)/home.style.tsx
+++ app/(home)/home.style.tsx
@@ -1,2 +1,31 @@
+"use client"
 import styled from "styled-components"
 
+export const HomeContainer = styled.div`
+    padding: 2rem;
+    height: 100vh;
+    position: relative;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    
+`
+
+export const ButtonContainer = styled.button`
+    position: absolute;
+    bottom: 2rem;
+    right: 2rem;
+
+    border: none;
+    border-radius: 50%;
+    height: 45px;
+    width: 45px;
+    color: white;
+    cursor: pointer;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: #03D69D;
+`
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,46 @@
-import Image from "next/image";
+"use client"
+import { useContext, useState } from "react";
 import Header from "../components/header/header";
+import { Input } from "../components/input/input";
+import { ButtonContainer, HomeContainer } from "./home.style";
+import { ArrowRight } from 'lucide-react';
+import { ValuePixContext } from "../context/value";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+  const { value, updateValue } = useContext(ValuePixContext);
+  const [inputValue, setInputValue] = useState<number>(0);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = parseFloat(event.target.value);
+    setInputValue(newValue);
+};
+
+const handleButtonClick = () => {
+    updateValue(inputValue);
+    setInputValue(0);
+    router.push(`/payment-method`);
+};
+
   return (
+    <HomeContainer>
     <div>
-      <Header title="Insira uma chave Pix" />
+        <Header title="Qual é o valor da transferência?" />
+        <Input
+            type="number"
+            placeholder="Ex: 1000"
+            onChange={handleChange}
+        />
     </div>
+
+    <div>
+        <p>Valor atual no contexto: {value}</p>
+    </div>
+
+    <ButtonContainer onClick={handleButtonClick}>
+        <ArrowRight size={25} />
+    </ButtonContainer>
+</HomeContainer>
   );
 }
