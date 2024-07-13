045981d83c3d606256f304e29d41b305b16324ee
Isaque Chaves <isaque.ramosch@gmail.com>
2024-07-13 16:14:08 -0300

    feat: ensure that the Payment Method page uses the CardValue component

--- app/payment-method/page.tsx
+++ app/payment-method/page.tsx
@@ -2,15 +2,28 @@
 import { useContext } from "react";
 import Header from "../components/header/header";
 import { ValuePixContext } from "../context/value";
-import { Container } from "../(home)/home.style";
+import { ButtonContainer, Container } from "../(home)/home.style";
+import { ArrowRight } from 'lucide-react';
+import CardValue from "../components/card-value/CardValue";
+import Footer from "../components/footer/Footer";
 
 const PaymentMethod = () => {
     const {value} = useContext(ValuePixContext);
 
     return ( 
         <Container>
-            <Header title="João, como você quer pagar ?" />
-            <p>{value}</p>
+            <div>
+                <Header title="João, como você quer pagar ?" />
+
+                <CardValue value={value} />
+
+            </div>
+
+            <Footer />
+            
+            <ButtonContainer>
+                <ArrowRight size={25} />
+            </ButtonContainer>
             
         </Container>
      );
