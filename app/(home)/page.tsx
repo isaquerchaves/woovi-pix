d9dfa4cab6db490b4ea79edc082c7b4342da7f80
Isaque Chaves <isaque.ramosch@gmail.com>
2024-07-13 11:48:03 -0300

    chore: add styled component lib

---
+++ app/(home)/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import Header from "../components/header/header";
+
+export default function Home() {
+  return (
+    <div>
+      <Header title="Insira uma chave Pix" />
+    </div>
+  );
+}
