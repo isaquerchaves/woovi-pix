17d5f629ea1974c899505892554d4e125389f2eb
Isaque Chaves <isaque.ramosch@gmail.com>
2024-07-14 16:40:35 -0300

    feat: add interest rate and installments in context

--- app/context/value.tsx
+++ app/context/value.tsx
@@ -1,26 +1,46 @@
-"use client"
+"use client";
 import { createContext, ReactNode, useState } from "react";
 
 interface ValuePix {
-    value: number;
-    updateValue: (newValue:number) => void;
+  value: number;
+  installmentCount: number;
+  interestRate: number; // Nova propriedade para a taxa de juros
+  updateValue: (newValue: number) => void;
+  updateInstallmentCount: (count: number) => void;
 }
 
 export const ValuePixContext = createContext<ValuePix>({
-    value: 0,
-    updateValue: (newValue: number) => {},
-})
+  value: 0,
+  installmentCount: 0,
+  interestRate: 1.99, // Valor padrão da taxa de juros
+  updateValue: (newValue: number) => {},
+  updateInstallmentCount: (count: number) => {},
+});
 
-export const ValuePixProvider = ({children}: {children: ReactNode}) => {
-    const [value, setValue] = useState<number>(0);
+export const ValuePixProvider = ({ children }: { children: ReactNode }) => {
+  const [value, setValue] = useState<number>(0);
+  const [installmentCount, setInstallmentCount] = useState<number>(0);
+  const interestRate = 1.99; // Definição da taxa de juros
 
-    const updateValue = (newValue: number) => {
-        setValue(newValue);
-    };
+  const updateValue = (newValue: number) => {
+    setValue(newValue);
+  };
 
-    return (
-        <ValuePixContext.Provider value={{value,updateValue }}>
-            {children}
-        </ValuePixContext.Provider>
-    )
-}
+  const updateInstallmentCount = (count: number) => {
+    setInstallmentCount(count);
+  };
+
+  return (
+    <ValuePixContext.Provider
+      value={{
+        value,
+        updateValue,
+        installmentCount,
+        updateInstallmentCount,
+        interestRate,
+      }}
+    >
+      {children}
+    </ValuePixContext.Provider>
+  );
+};
