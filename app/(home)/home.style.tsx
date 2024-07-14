93c7cdf264b2dc39929ca45d0af57200e775a0f1
Isaque Chaves <isaque.ramosch@gmail.com>
2024-07-14 16:46:05 -0300

    style: ensure that the Next button is fixed to the screen and decreasing the header height

--- app/(home)/home.style.tsx
+++ app/(home)/home.style.tsx
@@ -1,31 +1,28 @@
-"use client"
-import styled from "styled-components"
+"use client";
+import styled from "styled-components";
 
 export const Container = styled.div`
-    padding: 2rem;
-    height: 100vh;
-    position: relative;
+  padding: 2rem;
+  height: 100vh;
+  position: relative;
 
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    
-`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
 
 export const ButtonContainer = styled.button`
-    position: absolute;
-    bottom: 2rem;
-    right: 2rem;
-
-    border: none;
-    border-radius: 50%;
-    height: 45px;
-    width: 45px;
-    color: white;
-    cursor: pointer;
-
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    background: #03D69D;
-`
+  border: none;
+  border-radius: 50%;
+  height: 45px;
+  width: 45px;
+  color: white;
+  cursor: pointer;
+  position: fixed;
+  bottom: 1rem;
+  right: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #03d69d;
+`;
--- app/components/header/header.style.tsx
+++ app/components/header/header.style.tsx
@@ -1,17 +1,18 @@
-"use client"
+"use client";
 import styled from "styled-components";
 
 export const HeaderContent = styled.div`
-    height: 9rem;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    gap: 2rem;
+  height: 6rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+  margin-bottom: 1rem;
 
-    & p {
-        font-weight: 800;
-        font-size: 24px;
-        color: #4D4D4D;
-    }
-`
+  & p {
+    font-weight: 800;
+    font-size: 24px;
+    color: #4d4d4d;
+  }
+`;
