6809e8461bd9a352ad6ed2772c3239b7e05ddf6d
Isaque Chaves <isaque.ramosch@gmail.com>
2024-07-15 22:34:07 -0300

    feat: add payment card page

---
+++ app/payment-card/page.tsx
@@ -0,0 +1,241 @@
+"use client";
+import { useContext, useEffect, useState } from "react";
+import { ButtonContainer, Container } from "../(home)/home.style";
+import Header from "../components/header/header";
+import { ValuePixContext } from "../context/value";
+import {
+  calculateInstallmentWithInterest,
+  formatCurrency,
+} from "../helpers/value";
+import { useRouter } from "next/navigation";
+import { ArrowRight, Check, ChevronUp, Circle } from "lucide-react";
+import Footer from "../components/footer/Footer";
+import { PaymentCardContainer } from "./payment-card.style";
+import { PaymentContainer } from "../payment/payment.style";
+
+const PaymentCard = () => {
+  const router = useRouter();
+  const { value, installmentCount, interestRate, remainingValue } =
+    useContext(ValuePixContext);
+  const [formattedDate, setFormattedDate] = useState("");
+  const [installmentsInfo, setInstallmentsInfo] = useState<
+    { installment: number; value: number }[]
+  >([]);
+
+  const [cpf, setCpf] = useState("");
+  const [cardNumber, setCardNumber] = useState("");
+  const [expiryDate, setExpiryDate] = useState("");
+  const [cvv, setCvv] = useState("");
+
+  const remainingInstallments = installmentCount - 1;
+
+  // Calculo com o valor das parcelas
+  const valueCalculated = calculateInstallmentWithInterest(
+    value,
+    interestRate,
+    installmentCount
+  );
+
+  // Calculo com valor total
+  const valueTotalCalculated =
+    calculateInstallmentWithInterest(value, interestRate, installmentCount) *
+    installmentCount;
+
+  // Função para calcular o valor das parcelas do remainingValue
+  const calculateRemainingInstallments = () => {
+    const options = [];
+    for (let i = 1; i <= remainingInstallments; i++) {
+      const installmentValue = remainingValue / i;
+      options.push({ installment: i, value: installmentValue });
+    }
+    return options;
+  };
+
+  // Função para formatar a data no formato desejado e adicionar 1 hora
+  const formatDate = () => {
+    const currentDate = new Date();
+    currentDate.setHours(currentDate.getHours() + 1);
+    const formattedDate = currentDate.toLocaleDateString("pt-BR", {
+      day: "2-digit",
+      month: "2-digit",
+      year: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+    return formattedDate;
+  };
+
+  useEffect(() => {
+    setFormattedDate(formatDate());
+  }, []);
+
+  useEffect(() => {
+    // Gerar informações das parcelas restantes
+    const info = calculateRemainingInstallments();
+    setInstallmentsInfo(info);
+  }, [remainingValue]);
+
+  // Formatações dos inputs
+  const handleCardNumberChange = (e: any) => {
+    const value = e.target.value.replace(/\D/g, "");
+    const formattedCardNumber = value
+      .replace(/(\d{4})(\d)/, "$1 $2")
+      .replace(/(\d{4})(\d)/, "$1 $2")
+      .replace(/(\d{4})(\d)/, "$1 $2")
+      .replace(/(\d{4})(\d{1,4})$/, "$1 $2")
+      .substring(0, 19);
+    setCardNumber(formattedCardNumber);
+  };
+
+  const handleCpfChange = (e: any) => {
+    const value = e.target.value.replace(/\D/g, "");
+    const formattedCpf = value
+      .replace(/(\d{3})(\d)/, "$1.$2")
+      .replace(/(\d{3})(\d)/, "$1.$2")
+      .replace(/(\d{3})(\d{1,2})$/, "$1-$2")
+      .substring(0, 14);
+    setCpf(formattedCpf);
+  };
+
+  const handleExpiryDateChange = (e: any) => {
+    const value = e.target.value.replace(/\D/g, "");
+    const formattedDate = value.replace(/(\d{2})(\d)/, "$1/$2").substring(0, 7);
+    setExpiryDate(formattedDate);
+  };
+
+  const handleCvvChange = (e: any) => {
+    const value = e.target.value.replace(/\D/g, "").substring(0, 3);
+    setCvv(value);
+  };
+
+  const handlePaymentButtonClick = () => {
+    if (cpf && cardNumber && expiryDate && cvv) {
+      router.push(`/payment-successful`);
+    } else {
+      alert(
+        "Por favor, preencha todos os campos para prosseguir com o pagamento."
+      );
+    }
+  };
+
+  return (
+    <Container>
+      <Header
+        title={`Pague o restante em até ${remainingInstallments}x no cartão`}
+      />
+      <PaymentCardContainer>
+        <div className="form-field">
+          <input type="text" placeholder=" " />
+          <label>Nome Completo</label>
+        </div>
+        <div className="form-field">
+          <input
+            type="text"
+            placeholder=" "
+            value={cpf}
+            onChange={handleCpfChange}
+          />
+          <label>CPF</label>
+        </div>
+        <div className="form-field">
+          <input
+            type="text"
+            placeholder=" "
+            value={cardNumber}
+            onChange={handleCardNumberChange}
+          />
+          <label>Número do cartão</label>
+        </div>
+        <div className="vencimento-cvv">
+          <div className="form-field">
+            <input
+              type="text"
+              placeholder=" "
+              value={expiryDate}
+              onChange={handleExpiryDateChange}
+            />
+            <label>Vencimento</label>
+          </div>
+          <div className="form-field">
+            <input
+              type="text"
+              placeholder=" "
+              value={cvv}
+              onChange={handleCvvChange}
+            />
+            <label>CVV</label>
+          </div>
+        </div>
+        <div className="form-field">
+          <select>
+            {installmentsInfo.map((info) => (
+              <option key={info.installment} value={info.installment}>
+                {info.installment}x de{" "}
+                {formatCurrency(Number(info.value.toFixed(2)))}
+              </option>
+            ))}
+          </select>
+          <label>Parcelas</label>
+        </div>
+      </PaymentCardContainer>
+
+      <PaymentContainer>
+        <button className="copy-code" onClick={handlePaymentButtonClick}>
+          <div>Pagar</div>
+        </button>
+
+        <div className="identifier">
+          <p className="title">Prazo de pagamento:</p>
+          <p className="sub-title">{formattedDate}</p>
+        </div>
+
+        <div className="installments-section">
+          <div className="installments-itens">
+            <div className="pix">
+              <div className="circle-connector">
+                <Check
+                  size={16}
+                  color="white"
+                  style={{
+                    backgroundColor: "#03d69d",
+                    padding: "2px",
+                    borderRadius: "50%",
+                    border: "none",
+                  }}
+                />
+              </div>
+              <p>1ª entrada no Pix</p>
+            </div>
+            <span>{formatCurrency(valueCalculated)}</span>
+          </div>
+          <div className="installments-itens">
+            <div className="pix">
+              <Circle size={16} color="#E5E5E5" />
+              <p>2ª no cartão</p>
+            </div>
+            <span>{formatCurrency(remainingValue)}</span>
+          </div>
+        </div>
+
+        <div className="section-cet">
+          <p>CET: 0,5%</p>
+          <span>Total: {formatCurrency(valueTotalCalculated)}</span>
+        </div>
+
+        <div className="section-cet">
+          <span>Como funciona?</span>
+          <ChevronUp color="#4D4D4D" />
+        </div>
+
+        <div className="identifier">
+          <p className="title">Identifiador</p>
+          <p className="sub-title">2c1b951f356c4680b13ba1c9fc889c47</p>
+        </div>
+
+        <Footer />
+      </PaymentContainer>
+    </Container>
+  );
+};
+
+export default PaymentCard;
---
+++ app/payment-card/payment-card.style.tsx
@@ -0,0 +1,56 @@
+import styled from "styled-components";
+
+export const PaymentCardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  margin-bottom: 1rem;
+
+  .form-field {
+    position: relative;
+    margin-top: 0.5;
+  }
+
+  label {
+    position: absolute;
+    top: 0.75rem;
+    left: 0.75rem;
+    background-color: white;
+    padding: 0 0.25rem;
+    color: #4d4d4d;
+    font-size: 14px;
+    transition: 0.2s ease all;
+    pointer-events: none;
+  }
+
+  input,
+  select {
+    width: 100%;
+    padding: 1rem 0.5rem 0.5rem 0.5rem;
+    border: 1px solid #e5e5e5;
+    border-radius: 5px;
+    font-size: 14px;
+  }
+
+  input:focus,
+  input:not(:placeholder-shown),
+  select:focus,
+  select:not(:placeholder-shown) {
+    border: 1px solid #03d69d;
+  }
+
+  input:focus ~ label,
+  input:not(:placeholder-shown) ~ label,
+  select:focus ~ label,
+  select:not(:placeholder-shown) ~ label {
+    top: -0.5rem;
+    left: 0.5rem;
+    font-size: 12px;
+    color: #03d69d;
+  }
+
+  .vencimento-cvv {
+    display: flex;
+    gap: 1rem;
+  }
+`;
