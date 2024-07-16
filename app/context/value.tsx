5e11d081f242d444204398794d216da2232f98a3
Isaque Chaves <isaque.ramosch@gmail.com>
2024-07-15 22:33:39 -0300

    feat: add pending balance save function

--- app/context/value.tsx
+++ app/context/value.tsx
@@ -4,22 +4,27 @@ import { createContext, ReactNode, useState } from "react";
 interface ValuePix {
   value: number;
   installmentCount: number;
-  interestRate: number; // Nova propriedade para a taxa de juros
+  interestRate: number;
+  remainingValue: number;
   updateValue: (newValue: number) => void;
   updateInstallmentCount: (count: number) => void;
+  updateRemainingValue: (value: number) => void;
 }
 
 export const ValuePixContext = createContext<ValuePix>({
   value: 0,
   installmentCount: 0,
-  interestRate: 0.005, // Valor padrão da taxa de juros
+  interestRate: 0.005,
+  remainingValue: 0,
   updateValue: (newValue: number) => {},
   updateInstallmentCount: (count: number) => {},
+  updateRemainingValue: (value: number) => {},
 });
 
 export const ValuePixProvider = ({ children }: { children: ReactNode }) => {
   const [value, setValue] = useState<number>(0);
   const [installmentCount, setInstallmentCount] = useState<number>(0);
+  const [remainingValue, setRemainingValue] = useState<number>(0);
   const interestRate = 0.005; // Definição da taxa de juros
 
   const updateValue = (newValue: number) => {
@@ -30,6 +35,10 @@ export const ValuePixProvider = ({ children }: { children: ReactNode }) => {
     setInstallmentCount(count);
   };
 
+  const updateRemainingValue = (value: number) => {
+    setRemainingValue(value);
+  };
+
   return (
     <ValuePixContext.Provider
       value={{
@@ -38,6 +47,8 @@ export const ValuePixProvider = ({ children }: { children: ReactNode }) => {
         installmentCount,
         updateInstallmentCount,
         interestRate,
+        remainingValue,
+        updateRemainingValue,
       }}
     >
       {children}
